b70795daeb4fcbff7b31c8011e200bb938cfe6f8
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-07-02 15:20:19 -0300

    Ejercicios 1, 2 y 3
    curso de ingreso primera clase

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,19 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+
+
+// var person = prompt("Please enter your name", "Harry Potter");
+
+/* variable
+*/
+var nombre;
+
+nombre =prompt("ingrese su nombre", "lautaro");
+//usar siempre minuscula en alert
+//no olvidarse el punto y coma
+alert("su nombre es " +nombre);
+
 }
 
