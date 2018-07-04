d59e0d8bdc196c351d58112f4176334c6c702353
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-07-04 12:05:55 -0300

    1 y 2 del parcial

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,10 @@
 function mostrar()
 {
-  
+  var Nombre;
+  var localidad;
+Nombre=document.getElementById('elNombre').value;
+localidad=document.getElementById('laLocalidad').value;
+
+alert("usted es "+ Nombre+ " y vive en la localidad de "+localidad);
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+var base
 
+var altura
+
+var superficie
+
+var perimetro
+
+base=prompt("ingrese la base del triangulo")
+base=parseInt(base)
+perimetro=base*3
+altura=prompt("ingrese la altura del triangulo")
+altura=parseInt(altura)
+superficie= base*altura/2
+alert("el perimetro del triangulo es "+ perimetro+"y su altura es "+superficie )
 }
