23ac0a30308b8afca3ecd282e31d8a5d6d901a52
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-07-04 12:17:33 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -9,11 +9,11 @@ var superficie
 
 var perimetro
 
-base=prompt("ingrese la base del triangulo")
-base=parseInt(base)
-perimetro=base*3
-altura=prompt("ingrese la altura del triangulo")
-altura=parseInt(altura)
-superficie= base*altura/2
-alert("el perimetro del triangulo es "+ perimetro+"y su altura es "+superficie )
+base=prompt("ingrese la base del triangulo");
+base=parseInt(base);
+perimetro=base*3;
+altura=prompt("ingrese la altura del triangulo");
+altura=parseInt(altura);
+superficie=base*altura/2;
+alert("el perimetro del triangulo es "+ perimetro+" y su altura es "+superficie);
 }
