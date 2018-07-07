768a73e93cb42d739d34bcd9f9c2fddeb8d35b25
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-07-07 07:14:43 -0300

    ejercicios 1 2 y 3 del parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
 
+var precio;
+var descuento 
+var calculo;
+var final;
+precio=prompt("ingrese el precio");
+precio=parseInt(precio);
+descuento=prompt("ingrese el descuento");
+descuento=parseInt(descuento);
+//para convertir un numero a porcentaje se divide x100
+calculo=descuento/100;
+final=precio-precio*calculo;
+
+
+
+document.getElementById('elPrecioFinal').value=final;
+
+
+
+
+
+
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -2,4 +2,19 @@
 function mostrar()
 {
 
+var ancho;
+var largo;
+var perimetro;
+ancho=prompt("ingrese el ancho del rectangulo");
+ancho=parseInt(ancho);
+largo=prompt("ingrese el largo del rectangulo");
+largo=parseInt(largo);
+//usar parentesis cuando se combinan opereciones
+perimetro=2*(ancho+largo);
+
+alert("el perimeto del rectangulo es "+ perimetro);
+
+
+
+
 }
