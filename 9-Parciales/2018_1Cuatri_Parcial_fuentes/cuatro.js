68e436604c4465e6b55574999ba76740ccdb1d9f
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-07-14 12:16:42 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,60 +1,50 @@
-function mostrar()
-{
+	function mostrar()
+	{
 
-var numero1
-var numero2
-var resta
-var suma
-
-
-
-numero1=prompt("ingrese el primer numero")
-numero1=parseInt(numero1);
-
-numero2=prompt("ingrese el segundo numero");
-numero2=parseInt(numero2)
-
-resta=(numero1-numero2)
-
-suma=(numero1+numero2)
+	var numero1;
+	var numero2;
+	var suma;
+	var resta;
 
 
-if(numero1==numero2)
+	numero1=prompt("ingrese el primer numero");
+	numero1=parseInt(numero1);
 
-{
+	numero2=prompt("ingrese el segundo numero");
+	numero2=parseInt(numero2);
 
-alert(numero1+" y "+numero2);
-}
+	if(numero1==numero2)
+
+	{
+
+	 alert(numero1+" y "+numero2);
+
+	}
+	else 
+
+	 {	
+
+	    if(numero1<numero2)
+
+	   {
+	   	suma = numero1+numero2;
+	   	if(suma>10)
+	   	{
+	   		alert("la suma es mayor que 10");
+	   	}
+	    alert(suma);
+		}
+		else
+		{
+			resta = numero1-numero2;
+			alert(resta);
+		}
 
 
-   else if(numero1>numero2)
 
-
-   {
-
-   alert(resta);
-   }
-
-  else if(suma>10)
-
-
-   {
-
-   alert("hola");
-   }
-
-
- else
-
- {
-
- alert(suma);
- }
+	}
 
 
 
 
-
-
-
-}
+	}
