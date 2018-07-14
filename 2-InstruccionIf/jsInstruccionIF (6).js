22051dfd40e6f5b3914ea34fe7301ae9db755d7e
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-07-13 22:45:33 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -6,9 +6,63 @@ var edad;
 edad=document.getElementById('edad').value;
 edad=parseInt(edad);
 
-if(edad<13)
+if(edad<=13 && edad>=17)
+
 {
-	alert("es niño");
+
+ alert("es un adolescente")
+
 }
+
+else if  (edad>=18)
+
+{
+
+ alert("es un adulto")
+
+}
+
+else
+{
+
+ alert("es un niño")
+
+}
+
+
+
+
+
+
+/*
+if(edad>=18)
+{
+
+alert("es mayor de edad");
+
+}
+
+else
+{
+	
+  if (edad>=13 && edad<=17)
+
+  {
+
+  alert("es un adolescente");
+
+  }
+
+  else
+  {
+
+  alert("es niño");
+
+  }
+
+
+
+}
+*/
 
 }//FIN DE LA FUNCIÓN
