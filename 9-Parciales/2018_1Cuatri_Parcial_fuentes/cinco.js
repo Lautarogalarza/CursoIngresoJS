730da0f5e567f43f365cb3e70557e2eae2906885
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-07-25 02:22:43 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
 
+	var planetas;
+
+	planetas=prompt("ingrese el planeta");
+
+	switch(planetas)
+
+	{
+		case "mercurio":
+		case "venus":
+			alert("acá hace más calor");
+			break
+		case "tierra":
+			alert("aca vivimos")
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+			alert("aca hace mas frio");
+			break;
+			default:
+			alert("no es un planeta valido");
+			break;
+	}
+
+
+
+
 }
