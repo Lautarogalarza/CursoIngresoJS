f8ed2bd4657dff7dca99ba6c4c31bb1f8ae56dc2
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-07-25 12:23:23 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,35 +2,35 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-var nota=Math.floor((Math.random() * 10) + 1);
+   var nota=Math.floor((Math.random() * 10) + 1);
+   alert(nota);
 
-
-if(nota==9 || nota==10)
-
-{
-	alert("exelente");
-}
-
-else
-
-{
-
-	if(nota>4)
+   if(nota==9 || nota==10)
 
    {
-   	alert("aprobo");
+	  alert("exelente");
    }
 
-   else
+      else
 
-   {
-   	alert("vamos la proxima se puede");
-   }
+      {
+
+	     if(nota>4)
+
+         {
+   	     alert("aprobo");
+         }
+
+            else
+
+            {
+   	        alert("vamos la proxima se puede");
+            }
 
 
 
 
-}
+      }
 
 
 
