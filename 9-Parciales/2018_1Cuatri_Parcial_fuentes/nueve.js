3dc564c4ed9345f57e93b24fab3efdb9785bff19
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-08-05 22:04:34 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,98 @@
 function mostrar()
 {
 
+var marca;
+var peso;
+var temperatura;
+var producto;
+var respuesta="si";
+var cantidadDeTemperaturasPares=0;
+var cantidadDeProductosAceroGrados=0;
+var cantidadDePesos=0;
+var contadorDelPeso=0;
+var pesoMaximo;
+var marcaPesoMaximo;
+var pesoMinimo;
+var bandera=0;
+var promedio;
+while(respuesta=="si")
+	{
+		peso=prompt("ingrese el peso del producto(entre 1 y 100)");
+		peso=parseInt(peso);
+		cantidadDePesos=cantidadDePesos+peso
+		contadorDelPeso++
+			while(peso<1 || peso>100)
+			{
+				peso=prompt("error REingrese el peso del producto(entre 1 y 100)");
+				peso=parseInt(peso);
+		
+			}
+
+		temperatura=prompt("ingrese la temperatura de almacenamiento(entre -30 y 30)");
+		temperatura=parseInt(temperatura)
+
+			while(temperatura<-30 || temperatura>30)
+			{
+				temperatura=prompt("error REingrese la temperatura de almacenamiento(entre -30 y 30)");
+				temperatura=parseInt(temperatura);
+			}
+
+			marca=prompt("ingrese la marca del producto");
+
+
+
+			if(temperatura%2==0)
+			{
+				cantidadDeTemperaturasPares++
+			}
+
+
+			if(temperatura<0)
+			{
+				cantidadDeProductosAceroGrados++
+			}
+
+
+
+
+			if(peso>pesoMaximo || bandera==0)
+			{
+				pesoMaximo=peso;
+				marcaPesoMaximo=marca;
+			}
+
+			if(peso<pesoMinimo || bandera==0)
+			{
+				pesoMinimo=peso
+
+				bandera=1
+			}
+
+
+
+			respuesta=prompt("desea continuar?");
+
+
+
+
+	
+
+
+	}
+
+promedio=cantidadDePesos/contadorDelPeso;
+
+document.write("La cantidad de temperaturas pares es "+cantidadDeTemperaturasPares+"<br>");
+document.write("La marca del producto más pesado es "+marcaPesoMaximo+"<br>");
+document.write("La cantidad de productos que se conservan a menos de 0 grados son "+cantidadDeProductosAceroGrados+"<br>");
+document.write("El promedio del peso de todos los productos es "+promedio+"<br>");
+document.write("El peso máximo es "+pesoMaximo+" y el peso minimo es"+pesoMinimo+"<br>");
+
+
 }
+
+/*Testeo con los siguientes valores 
+(m=pepito;p = 50; t =-300(mal) ,-30(bien) ) 
+(m=teem;p = 10; t =0 ) 
+(m=llut;p = 150(mal), 15(bien); t =-13 ) 
+(m=fpy;p = 45; t =-12 )*/
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,133 @@
 function mostrar()
 {
 
+var letra;
+var numero;
+var respuesta="si";
+var contadorPares=0;
+var contadorImPares=0;
+var contadorDeCeros=0;
+var contadorPositivos=0;
+var acumuladorPositivos=0;
+var contadorNegativos=0;
+var acumuladorNegativos=0;
+var promedioPositivos;
+var sumaNegativos;
+var numeroMaximo;
+var letranumeroMaximo;
+var numeroMinimo;
+var letranumeroMinimo;
+var bandera=0;
+
+while(respuesta=="si")
+{
+	numero=prompt("ingrese un numero entre 100 y -100");
+	numero=parseInt(numero);
+	
+	while(numero<-100 || numero>100)
+	{
+		numero=prompt("error REingrese un numero entre 100 y -100");
+		numero=parseInt(numero);
+
+  	}
+
+  	letra=prompt("ingrese una letra");
+	letra=letra.toLowerCase();
+
+  	
+	if(numero%2==0 && numero!=0)
+	{
+		contadorPares++;
+	}
+
+	else
+	{
+		contadorImPares++
+	}
+
+	if(numero>0)
+		{
+			contadorPositivos++;
+			acumuladorPositivos=numero+acumuladorPositivos;
+		}
+
+	else if(numero<0)
+		{
+			contadorNegativos++;
+			acumuladorNegativos=numero+acumuladorNegativos;
+		}	
+
+	else 
+	{
+		contadorDeCeros++
+	}
+	/*else if(numero%2==1 || numero%2==-1)
+	{
+		contadorImPares++;
+	}
+
+	else
+	{
+		contadorDeCeros++
+	}
+
+
+	if(numero>0)
+		{
+			contadorPositivos++;
+			acumuladorPositivos=numero+acumuladorPositivos;
+		}
+
+	else
+		{
+			contadorNegativos++;
+			acumuladorNegativos=numero+acumuladorNegativos;
+		}
+
+
+*/
+
+		if(numero>numeroMaximo || bandera==0)
+			{
+				numeroMaximo=numero;
+				letranumeroMaximo=letra;
+			}
+
+
+		if(numero<numeroMinimo || bandera==0)
+		{
+			numeroMinimo=numero;
+			letranumeroMinimo=letra;
+
+			bandera=1;
+		}
+
+	respuesta=prompt("deasea continuar?");
+}
+
+promedioPositivos=acumuladorPositivos/contadorPositivos
+
+sumaNegativos=acumuladorNegativos
+
+document.write("la cantidad de numeros pares es "+contadorPares+"<br>");
+document.write("la cantidad de numeros Impares es "+contadorImPares+"<br>");
+document.write("la cantidad de ceros es "+contadorDeCeros+"<br>");
+	if(contadorPositivos==0)
+	{
+		promedioPositivos="No se ingreso ningun numero positivo"
+	}
+document.write("el promedio de los positivos es "+promedioPositivos+"<br>");
+	if(contadorNegativos==0)
+	{
+		sumaNegativos="No se ingreso ningun numero negativo"
+	}
+document.write("la suma de los negativos es "+sumaNegativos+"<br>");
+document.write("el numero maximo es "+numeroMaximo+" y su letra es "+letranumeroMaximo+"<br>");
+document.write("el numero minimo es "+numeroMinimo+" y su letra es "+letranumeroMinimo+"<br>");
+
+
+
+
+
+
 }
